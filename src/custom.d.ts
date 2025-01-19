7a3a8afa1cdb6fe028829475189a973890604516
Hyesun Kim <cocoabearhot@naver.com>
2025-01-20 01:09:37 +0900

    chore: custom.d.ts에 svg component 추가

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,5 +1,12 @@
-// src/custom.d.ts
 declare module "*.svg" {
-  const content: string;
-  export default content;
+  import * as React from "react";
+
+  // SVG를 React 컴포넌트로 사용하기 위한 타입
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  // SVG를 string 경로로 사용하기 위한 타입
+  const src: string;
+  export default src;
 }
